cca3f3ff46b3164639e92d953b31a5b1e76bddd9
Praneeth76 <praneethkandukuri76@gmail.com>
2025-04-10 14:12:11 +0530

    updated component

--- src/components/Inputbox.jsx
+++ src/components/Inputbox.jsx
@@ -24,7 +24,7 @@ function Inputbox({
         <p className='text-black/40 mb-2 w-full'>Currency Type</p>
         <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none' value={selectCurrency} onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)} disabled={currencyDisable}>
           {currencyOptions.map((currency) => (
-            <option key={currency} value={currency} >{currency}</option>
+            <option key={currency} value={currency} >{currency.toUpperCase()}</option>
           ))}
         </select>
       </div>
